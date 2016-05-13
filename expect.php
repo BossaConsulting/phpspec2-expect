7e088c1ac9f30677a8d2b1c72925d201eb6cf216
Blazej Pachut <blazej.pachut@gforces.pl>
2016-05-13 16:17:14 +0200

    Add option to disable expect so PHPSpec Expect can play along nice with Kahlan's expect

--- expect.php
+++ expect.php
@@ -29,7 +29,13 @@ use PhpSpec\Wrapper\Subject\WrappedObject;
 use PhpSpec\Wrapper\Unwrapper;
 use Symfony\Component\EventDispatcher\EventDispatcher;
 
-if (!function_exists('expect')) {
+$useExpect = true;
+
+if (getenv('PHPSPEC_DISABLE_EXPECT') || (defined('PHPSPEC_DISABLE_EXPECT') && PHPSPEC_DISABLE_EXPECT)) {
+    $useExpect = false;
+}
+
+if ($useExpect && !function_exists('expect')) {
     function expect($sus)
     {
         $presenter = new TaggedPresenter(new Differ);
